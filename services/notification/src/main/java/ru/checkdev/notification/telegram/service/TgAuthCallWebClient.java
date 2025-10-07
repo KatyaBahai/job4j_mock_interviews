f96ab6a52fbb99055490272ae6f360bf37c1a78c
KatyaBahai <katyxa-two@yandex.ru>
2025-10-07 15:54:05 +0500

    Telegram Bot [#505061]

--- services/notification/src/main/java/ru/checkdev/notification/telegram/service/TgAuthCallWebClient.java
+++ services/notification/src/main/java/ru/checkdev/notification/telegram/service/TgAuthCallWebClient.java
@@ -2,12 +2,14 @@ package ru.checkdev.notification.telegram.service;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import ru.checkdev.notification.domain.Profile;
+import ru.checkdev.notification.service.EurekaUriProvider;
 
 /**
  * Класс реализует методы get и post для отправки сообщений через WebClient
@@ -17,12 +19,14 @@ import ru.checkdev.notification.domain.Profile;
  */
 @org.springframework.context.annotation.Profile("default")
 @Service
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class TgAuthCallWebClient implements TgCall {
+    // "http://localhost:9900"
     @Value("${server.auth}")
-    private String urlServiceAuth;
+    private String serviceAuthId;
+    private final EurekaUriProvider uriProvider;
+
 
     /**
      * Метод get
@@ -32,7 +36,7 @@ public class TgAuthCallWebClient implements TgCall {
      */
     @Override
     public Mono<Profile> doGet(String url) {
-        return WebClient.create(urlServiceAuth)
+        return WebClient.create(uriProvider.getUri(serviceAuthId))
                 .get()
                 .uri(url)
                 .retrieve()
@@ -49,7 +53,7 @@ public class TgAuthCallWebClient implements TgCall {
      */
     @Override
     public Mono<Object> doPost(String url, Profile profile) {
-        return WebClient.create(urlServiceAuth)
+        return WebClient.create(uriProvider.getUri(serviceAuthId))
                 .post()
                 .uri(url)
                 .bodyValue(profile)
@@ -60,7 +64,7 @@ public class TgAuthCallWebClient implements TgCall {
 
     @Override
     public Mono<Object> doPost(String url) {
-        return WebClient.create(urlServiceAuth)
+        return WebClient.create(uriProvider.getUri(serviceAuthId))
                 .post()
                 .uri(url)
                 .retrieve()
