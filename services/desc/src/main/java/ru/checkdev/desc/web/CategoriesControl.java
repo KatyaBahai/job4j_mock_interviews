47f28f565bd192f74940cce84faa20ff445bc822
KatyaBahai <katyxa-two@yandex.ru>
2025-10-15 14:29:31 +0500

    swagger details and config for desc

--- services/desc/src/main/java/ru/checkdev/desc/web/CategoriesControl.java
+++ services/desc/src/main/java/ru/checkdev/desc/web/CategoriesControl.java
@@ -1,5 +1,6 @@
 package ru.checkdev.desc.web;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,10 @@ public class CategoriesControl {
         return categoryService.getAllCategoryDTO();
     }
 
+    @Operation(
+            summary = "Get most popular categories",
+            description = "Returns popular categories with the number of interviews."
+    )
     @GetMapping("/most_pop")
     public List<InterviewCountCategoryDto> getMostPopular() {
         return categoryService.getMostPopular();
